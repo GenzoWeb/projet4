3e7a04d711ef61062643b2b24974311efef20bb6
Genzo <genzo10@hotmail.fr>
2018-09-21 09:04:07 +0200

    Modification connexion base de donnée et lien htaccess pour mise en ligne

--- model/Manager.php
+++ model/Manager.php
@@ -3,7 +3,7 @@ class Manager
 {
 	protected function dbConnect()
 	{
-		$db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		$db = new PDO('mysql:host=db754366175.db.1and1.com;dbname=db754366175;charset=utf8', 'dbo754366175', 'forteroche', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         return $db;
 	}
 }
