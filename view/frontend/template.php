11438c0732fdcb4c872308528986628b4dbcc3c8
Genzo <genzo10@hotmail.fr>
2018-09-24 15:56:20 +0200

    Modification des balises <script> à l'intérieur du <head> (template.php)

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -20,8 +20,6 @@ unset($_SESSION['erreur']);
                 selector: '#mytextarea'
             });
         </script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
 
     <body>
